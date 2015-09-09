fe57dcb8946cc68ddf0bcfb92272274a997855fb
choplin <choplin.choplin@gmail.com>
2015-09-09 23:54:17 +0900

    build docker image using pgenv itself

--- Makefile
+++ Makefile
@@ -1,6 +1,15 @@
 src_dir := /go/src/github.com/choplin/pgenv
+go_srcs := $(shell find $(CURDIR) -name '*.go')
 
-.PHONY: test docker-test build-docker-image dev-test
+.PHONY: all clean test docker-test build-docker-image dev-test
+
+all: pgenv
+
+pgenv: $(go_srcs)
+	go build -v
+
+clean:
+	rm -f pgenv docker/pgenv
 
 test:
 	docker run --rm -v $(CURDIR):$(src_dir) -w $(src_dir) choplin/pgenv-test-env make docker-test
@@ -10,8 +19,11 @@ docker-test:
 	go build -v
 	go test ./...
 
-build-docker-image:
+build-docker-image: docker/pgenv
 	docker build --rm -t choplin/pgenv-test-env docker
+
+docker/pgenv: $(go_srcs)
+	GOOS=linux GOARCH=amd64 go build -o docker/pgenv
 
 dev-test:
 	docker run --rm -v $(GOPATH)/src:/go/src -w $(src_dir) choplin/pgenv-test-env go test --short ./...
--- command_list_test.go
+++ command_list_test.go
@@ -16,13 +16,15 @@ func ExampleListCommand_prettyDetail() {
 	app := makeTestEnv()
 	app.Run([]string{"pgenv", "list", "-f", "pretty", "-d"})
 	// Output:
-	// +-------------+------------------+---------------+------------------------------------------+-----------------------------------+--------------------------------------------+
-	// |    NAME     |     VERSION      | GIT REFERENCE |                   HASH                   |               PATH                |             CONFIGURE OPTIONS              |
-	// +-------------+------------------+---------------+------------------------------------------+-----------------------------------+--------------------------------------------+
-	// | 9.3.9-debug | PostgreSQL 9.3.9 | REL9_3_9      | 553e576e05b50f9faffbd3dd721e44fc3746898d | /root/.pgenv/versions/9.3.9-debug | --prefix=/root/.pgenv/versions/9.3.9-debug |
-	// |             |                  |               |                                          |                                   | --enable-debug --enable-cassert            |
-	// | 9.4.4       | PostgreSQL 9.4.4 | REL9_4_4      | 7c055f3ec3bd338a1ebb8c73cff3d01df626471e | /root/.pgenv/versions/9.4.4       | --prefix=/root/.pgenv/versions/9.4.4       |
-	// +-------------+------------------+---------------+------------------------------------------+-----------------------------------+--------------------------------------------+
+	// +-------------+------------------+---------------+------------------------------------------+--------------------------------------------+--------------------------------------------+
+	// |    NAME     |     VERSION      | GIT REFERENCE |                   HASH                   |                    PATH                    |             CONFIGURE OPTIONS              |
+	// +-------------+------------------+---------------+------------------------------------------+--------------------------------------------+--------------------------------------------+
+	// | 9.3.9-debug | PostgreSQL 9.3.9 | REL9_3_9      | 553e576e05b50f9faffbd3dd721e44fc3746898d | /home/postgres/.pgenv/versions/9.3.9-debug | --prefix                                   |
+	// |             |                  |               |                                          |                                            | /home/postgres/.pgenv/versions/9.3.9-debug |
+	// |             |                  |               |                                          |                                            | --enable-debug --enable-cassert            |
+	// | 9.4.4       | PostgreSQL 9.4.4 | REL9_4_4      | 7c055f3ec3bd338a1ebb8c73cff3d01df626471e | /home/postgres/.pgenv/versions/9.4.4       | --prefix                                   |
+	// |             |                  |               |                                          |                                            | /home/postgres/.pgenv/versions/9.4.4       |
+	// +-------------+------------------+---------------+------------------------------------------+--------------------------------------------+--------------------------------------------+
 }
 
 func ExampleListCommand_plain() {
@@ -39,8 +41,8 @@ func ExampleListCommand_plainDetail() {
 	app.Run([]string{"pgenv", "list", "-f", "plain", "-d"})
 	// Output:
 	// Name	Version	Git Reference	Hash	Path	Configure Options
-	// 9.3.9-debug	PostgreSQL 9.3.9	REL9_3_9	553e576e05b50f9faffbd3dd721e44fc3746898d	/root/.pgenv/versions/9.3.9-debug	--prefix=/root/.pgenv/versions/9.3.9-debug --enable-debug --enable-cassert
-	// 9.4.4	PostgreSQL 9.4.4	REL9_4_4	7c055f3ec3bd338a1ebb8c73cff3d01df626471e	/root/.pgenv/versions/9.4.4	--prefix=/root/.pgenv/versions/9.4.4
+	// 9.3.9-debug	PostgreSQL 9.3.9	REL9_3_9	553e576e05b50f9faffbd3dd721e44fc3746898d	/home/postgres/.pgenv/versions/9.3.9-debug	--prefix /home/postgres/.pgenv/versions/9.3.9-debug --enable-debug --enable-cassert
+	// 9.4.4	PostgreSQL 9.4.4	REL9_4_4	7c055f3ec3bd338a1ebb8c73cff3d01df626471e	/home/postgres/.pgenv/versions/9.4.4	--prefix /home/postgres/.pgenv/versions/9.4.4
 }
 func ExampleListCommand_json() {
 	app := makeTestEnv()
@@ -51,5 +53,5 @@ func ExampleListCommand_json() {
 func ExampleListCommand_jsonDetail() {
 	app := makeTestEnv()
 	app.Run([]string{"pgenv", "list", "-f", "json", "-d"})
-	// Output: [{"name":"9.3.9-debug","version":"PostgreSQL 9.3.9","git-ref":"REL9_3_9","hash":"553e576e05b50f9faffbd3dd721e44fc3746898d","path":"/root/.pgenv/versions/9.3.9-debug","configureOptions":["--prefix=/root/.pgenv/versions/9.3.9-debug","--enable-debug","--enable-cassert"]},{"name":"9.4.4","version":"PostgreSQL 9.4.4","git-ref":"REL9_4_4","hash":"7c055f3ec3bd338a1ebb8c73cff3d01df626471e","path":"/root/.pgenv/versions/9.4.4","configureOptions":["--prefix=/root/.pgenv/versions/9.4.4"]}]
+	// Output: [{"name":"9.3.9-debug","version":"PostgreSQL 9.3.9","git-ref":"REL9_3_9","hash":"553e576e05b50f9faffbd3dd721e44fc3746898d","path":"/home/postgres/.pgenv/versions/9.3.9-debug","configureOptions":["--prefix","/home/postgres/.pgenv/versions/9.3.9-debug","--enable-debug","--enable-cassert"]},{"name":"9.4.4","version":"PostgreSQL 9.4.4","git-ref":"REL9_4_4","hash":"7c055f3ec3bd338a1ebb8c73cff3d01df626471e","path":"/home/postgres/.pgenv/versions/9.4.4","configureOptions":["--prefix","/home/postgres/.pgenv/versions/9.4.4"]}]
 }
